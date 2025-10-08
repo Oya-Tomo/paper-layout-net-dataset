d220591f7183540be11887e8916cbb734fe922b3
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2025-10-08 18:07:52 +0900

    update: add batch prediction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import random
+from dataclasses import dataclass
 
 import torch
 import yaml
@@ -15,7 +16,16 @@ from collector import (
 from model import load_yolo_model
 
 
-def main():
+@dataclass
+class DatasetConfig:
+    dpi: int = 150
+    batch_size: int = 15
+    train_split: float = 0.7
+    val_split: float = 0.15
+    test_split: float = 0.15
+
+
+def main(config: DatasetConfig = DatasetConfig()):
     os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 
     os.makedirs("dataset/images/train", exist_ok=True)
@@ -76,6 +86,15 @@ def main():
     id_list = None
     start = 0
     max_results = 20
+
+    page_pool: list[
+        tuple[
+            str,
+            int,
+            Image.Image,
+            str,
+        ]
+    ] = []  # contains (hash, page, image, usage)
     for query in queries:
         papers = collect_arxiv_papers(
             search_query=query,
@@ -90,24 +109,41 @@ def main():
             if not download_pdf(paper.pdf, pdf_path):
                 continue
 
-            images = get_pdf_page_images(pdf_path, dpi=150)
-            images_predict = model.predict(images)
+            images = get_pdf_page_images(pdf_path, dpi=config.dpi)
             images_usage = random.choices(
                 ["train", "val", "test"],
-                weights=[0.7, 0.15, 0.15],
+                weights=[config.train_split, config.val_split, config.test_split],
                 k=len(images),
             )
+            for idx, image in enumerate(images):
+                page_pool.append((paper_hash, idx, image, images_usage[idx]))
 
-            for i in range(len(images)):
-                image_id = f"{paper_hash}_{i}"
+            if len(page_pool) < config.batch_size:
+                continue
 
-                images[i].save(f"dataset/images/{images_usage[i]}/{image_id}.png")
-                results = images_predict[i]
-                with open(f"dataset/labels/{images_usage[i]}/{image_id}.txt", "w") as f:
-                    for result in results.boxes:
-                        class_id = result.cls.item()
-                        x_center, y_center, width, height = result.xywhn[0].tolist()
-                        f.write(f"{class_id} {x_center} {y_center} {width} {height}\n")
+            for _ in range(len(page_pool) // config.batch_size):
+                batch = page_pool[: config.batch_size]
+                page_pool = page_pool[config.batch_size :]
+
+                paper_hashes, page_indices, images, images_usage = zip(*batch)
+                images = list(images)
+                images_usage = list(images_usage)
+                images_predict = model.predict(images)
+
+                for i in range(len(images)):
+                    image_id = f"{paper_hashes[i]}_{page_indices[i]}"
+
+                    images[i].save(f"dataset/images/{images_usage[i]}/{image_id}.png")
+                    results = images_predict[i]
+                    with open(
+                        f"dataset/labels/{images_usage[i]}/{image_id}.txt", "w"
+                    ) as f:
+                        for result in results.boxes:
+                            class_id = int(result.cls.item())
+                            x_center, y_center, width, height = result.xywhn[0].tolist()
+                            f.write(
+                                f"{class_id} {x_center} {y_center} {width} {height}\n"
+                            )
 
             os.remove(pdf_path)
             del images
