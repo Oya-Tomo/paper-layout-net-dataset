9657708de8b92662f9c360f1b691feceee1feb99
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2025-10-08 14:00:32 +0900

    update: add test argparser

--- test.py
+++ test.py
@@ -1,21 +1,37 @@
+import argparse
+
 from ultralytics.data.utils import visualize_image_annotations
 
-label_map = (
-    {
-        0: "Caption",
-        1: "Footnote",
-        2: "Formula",
-        3: "List-item",
-        4: "Page-footer",
-        5: "Page-header",
-        6: "Picture",
-        7: "Section-header",
-        8: "Table",
-        9: "Text",
-        10: "Title",
-    },
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--img_path",
+    type=str,
+    default="dataset/images/test/0.png",
 )
+parser.add_argument(
+    "--label_path",
+    type=str,
+    default="dataset/labels/test/0.txt",
+)
+args = parser.parse_args()
+
+label_map = {
+    0: "Caption",
+    1: "Footnote",
+    2: "Formula",
+    3: "List-item",
+    4: "Page-footer",
+    5: "Page-header",
+    6: "Picture",
+    7: "Section-header",
+    8: "Table",
+    9: "Text",
+    10: "Title",
+}
+
 
 visualize_image_annotations(
+    args.img_path,
+    args.label_path,
     label_map=label_map,
 )
